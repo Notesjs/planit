9349242ad4ffc09cf8fb3eddde5885e1af7204da
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-10-28 22:27:11 -0700

    [fix] - git conflicts

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,9 +3,6 @@ module.exports = {
   activityController: require('./activity.controller'),
   searchController: require('./search.controller'),
   authController: require('./auth.controller'),
-<<<<<<< HEAD
   flightController: require('./flight.controller')
-=======
   chatsController: require('./chats.controller')
->>>>>>> 9567fcfa4bad106eba7fd5238f268c10e9c35e98
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,12 +5,8 @@ var activityRouter = require('./activity.router');
 var searchRouter = require('./search.router');
 var authRouter = require('./auth.router');
 var paymentRouter = require('./payment.router')
-<<<<<<< HEAD
 var flightRouter = require('./flight.router');
-=======
 var chatsRouter = require('./chats.router')
->>>>>>> 9567fcfa4bad106eba7fd5238f268c10e9c35e98
-
 var router = express.Router();
 
 router.use('/itinerary', itineraryRouter);
@@ -18,10 +14,7 @@ router.use('/activity', activityRouter);
 router.use('/search', searchRouter);
 router.use('/auth', authRouter);
 router.use('/payment', paymentRouter)
-<<<<<<< HEAD
 router.use('/flight', flightRouter);
-=======
 router.use('/chats', chatsRouter)
->>>>>>> 9567fcfa4bad106eba7fd5238f268c10e9c35e98
 
 module.exports = router;
